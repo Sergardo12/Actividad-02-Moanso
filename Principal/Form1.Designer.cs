1f62eb789d4285b2d6cb4219f0accdcbf0af7b4e
Sergardo12 <sergardo12@gmail.com>
2024-08-26 18:09:51 -0500

    Inserción de la interfaz principal

---
+++ Principal/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Principal
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEjer01 = new System.Windows.Forms.Button();
+            this.btnEjer02 = new System.Windows.Forms.Button();
+            this.btnEjer03 = new System.Windows.Forms.Button();
+            this.btnEjer04 = new System.Windows.Forms.Button();
+            this.btnEjer06 = new System.Windows.Forms.Button();
+            this.btnEjer05 = new System.Windows.Forms.Button();
+            this.btnEjer07 = new System.Windows.Forms.Button();
+            this.btnEjer08 = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat ExtraBold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(233, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(326, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ACTIVIDAD 02 - MOANSO";
+            // 
+            // btnEjer01
+            // 
+            this.btnEjer01.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnEjer01.Location = new System.Drawing.Point(239, 149);
+            this.btnEjer01.Name = "btnEjer01";
+            this.btnEjer01.Size = new System.Drawing.Size(132, 23);
+            this.btnEjer01.TabIndex = 1;
+            this.btnEjer01.Text = "EJERCICIO 01";
+            this.btnEjer01.UseVisualStyleBackColor = true;
+            this.btnEjer01.Click += new System.EventHandler(this.btnEjer01_Click);
+            // 
+            // btnEjer02
+            // 
+            this.btnEjer02.Location = new System.Drawing.Point(472, 149);
+            this.btnEjer02.Name = "btnEjer02";
+            this.btnEjer02.Size = new System.Drawing.Size(132, 23);
+            this.btnEjer02.TabIndex = 2;
+            this.btnEjer02.Text = "EJERCICIO 02";
+            this.btnEjer02.UseVisualStyleBackColor = true;
+            // 
+            // btnEjer03
+            // 
+            this.btnEjer03.Location = new System.Drawing.Point(239, 208);
+            this.btnEjer03.Name = "btnEjer03";
+            this.btnEjer03.Size = new System.Drawing.Size(132, 23);
+            this.btnEjer03.TabIndex = 3;
+            this.btnEjer03.Text = "EJERCICIO 03";
+            this.btnEjer03.UseVisualStyleBackColor = true;
+            // 
+            // btnEjer04
+            // 
+            this.btnEjer04.Location = new System.Drawing.Point(474, 208);
+            this.btnEjer04.Name = "btnEjer04";
+            this.btnEjer04.Size = new System.Drawing.Size(130, 23);
+            this.btnEjer04.TabIndex = 4;
+            this.btnEjer04.Text = "EJERCICIO 04";
+            this.btnEjer04.UseVisualStyleBackColor = true;
+            // 
+            // btnEjer06
+            // 
+            this.btnEjer06.Location = new System.Drawing.Point(474, 263);
+            this.btnEjer06.Name = "btnEjer06";
+            this.btnEjer06.Size = new System.Drawing.Size(130, 23);
+            this.btnEjer06.TabIndex = 5;
+            this.btnEjer06.Text = "EJERCICIO 06";
+            this.btnEjer06.UseVisualStyleBackColor = true;
+            // 
+            // btnEjer05
+            // 
+            this.btnEjer05.Location = new System.Drawing.Point(239, 263);
+            this.btnEjer05.Name = "btnEjer05";
+            this.btnEjer05.Size = new System.Drawing.Size(132, 23);
+            this.btnEjer05.TabIndex = 6;
+            this.btnEjer05.Text = "EJERCICIO 05";
+            this.btnEjer05.UseVisualStyleBackColor = true;
+            // 
+            // btnEjer07
+            // 
+            this.btnEjer07.Location = new System.Drawing.Point(239, 323);
+            this.btnEjer07.Name = "btnEjer07";
+            this.btnEjer07.Size = new System.Drawing.Size(132, 23);
+            this.btnEjer07.TabIndex = 7;
+            this.btnEjer07.Text = "EJERCICIO 07";
+            this.btnEjer07.UseVisualStyleBackColor = true;
+            // 
+            // btnEjer08
+            // 
+            this.btnEjer08.Location = new System.Drawing.Point(474, 323);
+            this.btnEjer08.Name = "btnEjer08";
+            this.btnEjer08.Size = new System.Drawing.Size(130, 23);
+            this.btnEjer08.TabIndex = 8;
+            this.btnEjer08.Text = "EJERCICIO 08";
+            this.btnEjer08.UseVisualStyleBackColor = true;
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.BackColor = System.Drawing.Color.Red;
+            this.btnCerrar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnCerrar.Location = new System.Drawing.Point(360, 383);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(91, 27);
+            this.btnCerrar.TabIndex = 9;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = false;
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(34)))), ((int)(((byte)(54)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnEjer08);
+            this.Controls.Add(this.btnEjer07);
+            this.Controls.Add(this.btnEjer05);
+            this.Controls.Add(this.btnEjer06);
+            this.Controls.Add(this.btnEjer04);
+            this.Controls.Add(this.btnEjer03);
+            this.Controls.Add(this.btnEjer02);
+            this.Controls.Add(this.btnEjer01);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormPrincipal";
+            this.Text = "ACTIVIDAD 02 - MOANSO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEjer01;
+        private System.Windows.Forms.Button btnEjer02;
+        private System.Windows.Forms.Button btnEjer03;
+        private System.Windows.Forms.Button btnEjer04;
+        private System.Windows.Forms.Button btnEjer06;
+        private System.Windows.Forms.Button btnEjer05;
+        private System.Windows.Forms.Button btnEjer07;
+        private System.Windows.Forms.Button btnEjer08;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
+
---
+++ Principal/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Principal
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEjer01_Click(object sender, EventArgs e)
+        {
+           InscripcionTalleres.Form1 form1 = new InscripcionTalleres.Form1();
+        }
+    }
+}
