c1787c193f73e0ef35e8ad3ff98270bc8ab87176
eduar <eduar@DESKTOP-12S2Q56>
2024-08-26 17:23:34 -0500

    Commit ejercicio fab

--- Ejercicio02/TV.Designer.cs
+++ Ejercicio02/TV.Designer.cs
@@ -28,33 +28,232 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnIngresarGeneros = new System.Windows.Forms.Button();
+            this.txtGenero = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnEliminarSubGenero = new System.Windows.Forms.Button();
+            this.btnEliminarGenero = new System.Windows.Forms.Button();
+            this.comboBoxEliminarSubGeneros = new System.Windows.Forms.ComboBox();
+            this.comboBoxEliminarGeneros = new System.Windows.Forms.ComboBox();
+            this.treeViewLibros = new System.Windows.Forms.TreeView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBoxConGeneros = new System.Windows.Forms.ComboBox();
+            this.txtSubGenero = new System.Windows.Forms.TextBox();
+            this.btnIngresarSubGeneros = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // treeView1
+            // label1
             // 
-            this.treeView1.Location = new System.Drawing.Point(12, 12);
-            this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(227, 269);
-            this.treeView1.TabIndex = 0;
-            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(88, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Ingrese un género";
             // 
-            // Form1
+            // btnIngresarGeneros
+            // 
+            this.btnIngresarGeneros.Location = new System.Drawing.Point(102, 135);
+            this.btnIngresarGeneros.Name = "btnIngresarGeneros";
+            this.btnIngresarGeneros.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresarGeneros.TabIndex = 19;
+            this.btnIngresarGeneros.Text = "Ingresar";
+            this.btnIngresarGeneros.UseVisualStyleBackColor = true;
+            this.btnIngresarGeneros.Click += new System.EventHandler(this.btnIngresarGeneros_Click);
+            // 
+            // txtGenero
+            // 
+            this.txtGenero.Location = new System.Drawing.Point(91, 109);
+            this.txtGenero.Name = "txtGenero";
+            this.txtGenero.Size = new System.Drawing.Size(100, 20);
+            this.txtGenero.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(36, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(650, 24);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "INGRESAR GÉNEROS Y SUBGÉNEROS DE LIBROS EN UNA BIBLIOTECA";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(513, 353);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(205, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Seleccione el subgénero a eliminar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(308, 353);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(185, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Seleccione el género a eliminar";
+            // 
+            // btnEliminarSubGenero
+            // 
+            this.btnEliminarSubGenero.Location = new System.Drawing.Point(516, 396);
+            this.btnEliminarSubGenero.Name = "btnEliminarSubGenero";
+            this.btnEliminarSubGenero.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminarSubGenero.TabIndex = 23;
+            this.btnEliminarSubGenero.Text = "Eliminar";
+            this.btnEliminarSubGenero.UseVisualStyleBackColor = true;
+            this.btnEliminarSubGenero.Click += new System.EventHandler(this.btnEliminarSubGenero_Click);
+            // 
+            // btnEliminarGenero
+            // 
+            this.btnEliminarGenero.Location = new System.Drawing.Point(311, 396);
+            this.btnEliminarGenero.Name = "btnEliminarGenero";
+            this.btnEliminarGenero.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminarGenero.TabIndex = 22;
+            this.btnEliminarGenero.Text = "Eliminar";
+            this.btnEliminarGenero.UseVisualStyleBackColor = true;
+            this.btnEliminarGenero.Click += new System.EventHandler(this.btnEliminarGenero_Click);
+            // 
+            // comboBoxEliminarSubGeneros
+            // 
+            this.comboBoxEliminarSubGeneros.FormattingEnabled = true;
+            this.comboBoxEliminarSubGeneros.Location = new System.Drawing.Point(516, 369);
+            this.comboBoxEliminarSubGeneros.Name = "comboBoxEliminarSubGeneros";
+            this.comboBoxEliminarSubGeneros.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxEliminarSubGeneros.TabIndex = 21;
+            // 
+            // comboBoxEliminarGeneros
+            // 
+            this.comboBoxEliminarGeneros.FormattingEnabled = true;
+            this.comboBoxEliminarGeneros.Location = new System.Drawing.Point(311, 369);
+            this.comboBoxEliminarGeneros.Name = "comboBoxEliminarGeneros";
+            this.comboBoxEliminarGeneros.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxEliminarGeneros.TabIndex = 20;
+            // 
+            // treeViewLibros
+            // 
+            this.treeViewLibros.Location = new System.Drawing.Point(312, 65);
+            this.treeViewLibros.Name = "treeViewLibros";
+            this.treeViewLibros.Size = new System.Drawing.Size(406, 274);
+            this.treeViewLibros.TabIndex = 17;
+            this.treeViewLibros.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeViewLibros_AfterSelect);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBoxConGeneros);
+            this.groupBox1.Controls.Add(this.txtSubGenero);
+            this.groupBox1.Controls.Add(this.btnIngresarSubGeneros);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(8, 196);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(294, 158);
+            this.groupBox1.TabIndex = 28;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingresar subgéneros en los géneros";
+            // 
+            // comboBoxConGeneros
+            // 
+            this.comboBoxConGeneros.FormattingEnabled = true;
+            this.comboBoxConGeneros.Location = new System.Drawing.Point(24, 44);
+            this.comboBoxConGeneros.Name = "comboBoxConGeneros";
+            this.comboBoxConGeneros.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxConGeneros.TabIndex = 5;
+            // 
+            // txtSubGenero
+            // 
+            this.txtSubGenero.Location = new System.Drawing.Point(24, 95);
+            this.txtSubGenero.Name = "txtSubGenero";
+            this.txtSubGenero.Size = new System.Drawing.Size(100, 20);
+            this.txtSubGenero.TabIndex = 2;
+            // 
+            // btnIngresarSubGeneros
+            // 
+            this.btnIngresarSubGeneros.Location = new System.Drawing.Point(24, 121);
+            this.btnIngresarSubGeneros.Name = "btnIngresarSubGeneros";
+            this.btnIngresarSubGeneros.Size = new System.Drawing.Size(75, 23);
+            this.btnIngresarSubGeneros.TabIndex = 4;
+            this.btnIngresarSubGeneros.Text = "Ingresar";
+            this.btnIngresarSubGeneros.UseVisualStyleBackColor = true;
+            this.btnIngresarSubGeneros.Click += new System.EventHandler(this.btnIngresarSubGeneros_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Ingrese el Subgénero";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(272, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Seleccione el género para el nuevo subgénero";
+            // 
+            // Ejercicio05
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(250, 305);
-            this.Controls.Add(this.treeView1);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(738, 441);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnIngresarGeneros);
+            this.Controls.Add(this.txtGenero);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnEliminarSubGenero);
+            this.Controls.Add(this.btnEliminarGenero);
+            this.Controls.Add(this.comboBoxEliminarSubGeneros);
+            this.Controls.Add(this.comboBoxEliminarGeneros);
+            this.Controls.Add(this.treeViewLibros);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Ejercicio05";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnIngresarGeneros;
+        private System.Windows.Forms.TextBox txtGenero;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnEliminarSubGenero;
+        private System.Windows.Forms.Button btnEliminarGenero;
+        private System.Windows.Forms.ComboBox comboBoxEliminarSubGeneros;
+        private System.Windows.Forms.ComboBox comboBoxEliminarGeneros;
+        private System.Windows.Forms.TreeView treeViewLibros;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBoxConGeneros;
+        private System.Windows.Forms.TextBox txtSubGenero;
+        private System.Windows.Forms.Button btnIngresarSubGeneros;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Ejercicio02/TV.cs
+++ Ejercicio02/TV.cs
@@ -13,48 +13,180 @@ namespace Ejercicio02
 {
     public partial class Ejercicio05 : Form
     {
+        private string connectionString = "Server=DESKTOP-12S2Q56;Database=empresa;Trusted_Connection=True;TrustServerCertificate=True;";
         public Ejercicio05()
         {
             InitializeComponent();
         }
 
-        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
-        {
-          
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
           
-            string connectionString = "Server=DESKTOP-12S2Q56;Database=empresa;Trusted_Connection=True;";
+           
+        }
 
-            
-            string query = "SELECT Codigo, Nombre, Apellido, Direccion FROM cliente";
+        private void treeViewLibros_AfterSelect(object sender, TreeViewEventArgs e)
+        {
 
-         
-            using (SqlConnection connection = new SqlConnection(connectionString))
+        }
+
+        private void btnEliminarGenero_Click(object sender, EventArgs e)
+        {
+            if (comboBoxEliminarGeneros.SelectedItem != null)
             {
-                SqlCommand command = new SqlCommand(query, connection);
-                connection.Open();
+                string selectedGenre = comboBoxEliminarGeneros.SelectedItem.ToString();
 
-                SqlDataReader reader = command.ExecuteReader();
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT Id FROM Generos WHERE Nombre = @Nombre", conn);
+                    cmd.Parameters.AddWithValue("@Nombre", selectedGenre);
+                    int genreId = (int)cmd.ExecuteScalar();
+                    cmd = new SqlCommand("DELETE FROM SubGeneros WHERE GeneroId = @GeneroId", conn);
+                    cmd.Parameters.AddWithValue("@GeneroId", genreId);
+                    cmd.ExecuteNonQuery();
+
+
+                    cmd = new SqlCommand("DELETE FROM Generos WHERE Id = @Id", conn);
+                    cmd.Parameters.AddWithValue("@Id", genreId);
+                    cmd.ExecuteNonQuery();
+
+
+                    LoadGenres();
+                }
+            }
+        }
+        private void LoadGenres()
+        {
+            treeViewLibros.Nodes.Clear();
+            comboBoxConGeneros.Items.Clear();
+            comboBoxEliminarGeneros.Items.Clear();
+            comboBoxEliminarSubGeneros.Items.Clear();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Generos", conn);
+                SqlDataReader reader = cmd.ExecuteReader();
 
                 while (reader.Read())
                 {
-                 
-                    TreeNode clientNode = new TreeNode($"Cliente {reader["Codigo"]}");
+                    TreeNode genreNode = new TreeNode(reader["Nombre"].ToString())
+                    {
+                        Tag = reader["Id"]
+                    };
 
-                 
-                    clientNode.Nodes.Add($"Nombre: {reader["Nombre"]}");
-                    clientNode.Nodes.Add($"Apellido: {reader["Apellido"]}");
-                    clientNode.Nodes.Add($"Dirección: {reader["Direccion"]}");
-
-                    treeView1.Nodes.Add(clientNode);
+                    treeViewLibros.Nodes.Add(genreNode);
+                    comboBoxConGeneros.Items.Add(reader["Nombre"].ToString());
+                    comboBoxEliminarGeneros.Items.Add(reader["Nombre"].ToString());
                 }
 
                 reader.Close();
+
+                foreach (TreeNode genreNode in treeViewLibros.Nodes)
+                {
+                    int genreId = (int)genreNode.Tag;
+
+                    cmd = new SqlCommand("SELECT * FROM SubGeneros WHERE GeneroId = @GeneroId", conn);
+                    cmd.Parameters.AddWithValue("@GeneroId", genreId);
+                    reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        TreeNode subGenreNode = new TreeNode(reader["Nombre"].ToString())
+                        {
+                            Tag = reader["Id"]
+                        };
+
+                        genreNode.Nodes.Add(subGenreNode);
+                        comboBoxEliminarSubGeneros.Items.Add($"{subGenreNode.Text} - {genreNode.Text}");
+                    }
+
+                    reader.Close();
+                }
             }
         }
 
+        private void btnIngresarGeneros_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtGenero.Text))
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO Generos (Nombre) VALUES (@Nombre); SELECT SCOPE_IDENTITY();", conn);
+                    cmd.Parameters.AddWithValue("@Nombre", txtGenero.Text);
+                    int newGenreId = Convert.ToInt32(cmd.ExecuteScalar());
+
+                    TreeNode newGenre = new TreeNode(txtGenero.Text)
+                    {
+                        Tag = newGenreId
+                    };
+
+                    treeViewLibros.Nodes.Add(newGenre);
+                    comboBoxConGeneros.Items.Add(newGenre.Text);
+                    comboBoxEliminarGeneros.Items.Add(newGenre.Text);
+
+                    txtGenero.Clear();
+                }
+            }
+        }
+
+        private void btnIngresarSubGeneros_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtSubGenero.Text) && comboBoxConGeneros.SelectedItem != null)
+            {
+                string selectedGenre = comboBoxConGeneros.SelectedItem.ToString();
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT Id FROM Generos WHERE Nombre = @Nombre", conn);
+                    cmd.Parameters.AddWithValue("@Nombre", selectedGenre);
+                    int genreId = (int)cmd.ExecuteScalar();
+
+                    cmd = new SqlCommand("INSERT INTO SubGeneros (Nombre, GeneroId) VALUES (@Nombre, @GeneroId); SELECT SCOPE_IDENTITY();", conn);
+                    cmd.Parameters.AddWithValue("@Nombre", txtSubGenero.Text);
+                    cmd.Parameters.AddWithValue("@GeneroId", genreId);
+                    cmd.ExecuteNonQuery();
+
+                    LoadGenres();
+                    txtSubGenero.Clear();
+                }
+            }
+        }
+
+        private void btnEliminarSubGenero_Click(object sender, EventArgs e)
+        {
+            if (comboBoxEliminarSubGeneros.SelectedItem != null)
+            {
+                string selectedItem = comboBoxEliminarSubGeneros.SelectedItem.ToString();
+                string[] parts = selectedItem.Split('-');
+                string selectedSubGenre = parts[0].Trim();
+                string selectedGenre = parts[1].Trim();
+
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+
+                    SqlCommand cmd = new SqlCommand("SELECT Id FROM SubGeneros WHERE Nombre = @Nombre AND GeneroId = (SELECT Id FROM Generos WHERE Nombre = @Genero)", conn);
+                    cmd.Parameters.AddWithValue("@Nombre", selectedSubGenre);
+                    cmd.Parameters.AddWithValue("@Genero", selectedGenre);
+                    int subGenreId = (int)cmd.ExecuteScalar();
+
+
+                    cmd = new SqlCommand("DELETE FROM SubGeneros WHERE Id = @Id", conn);
+                    cmd.Parameters.AddWithValue("@Id", subGenreId);
+                    cmd.ExecuteNonQuery();
+
+                    LoadGenres();
+                }
+            }
+        }
     }
 }
